4c7d9c677b91e95a040139b68ac5dd9d317c54de
fadilghulam <razerstarsdota@gmail.com>
2016-11-03 10:15:11 +0700

    Check Push Error

--- app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl305152535/mokletvoter/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl305152535/mokletvoter/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.d(TAG, "onCreate: testing");
+        Log.d("coba", "onCreate: testing");
     }
 }
